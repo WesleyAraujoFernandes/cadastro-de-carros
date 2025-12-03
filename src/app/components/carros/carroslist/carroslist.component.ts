e56a431faf439c6d8cbb4479fc2d272525af4695
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2025-12-03 17:50:53 -0300

    car details creation

--- src/app/components/carros/carroslist/carroslist.component.ts
+++ src/app/components/carros/carroslist/carroslist.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Carro } from '../../../models/carro';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-carroslist',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './carroslist.component.html',
   styleUrl: './carroslist.component.scss'
 })
 export class CarroslistComponent {
+  lista: Carro[] = [];
 
+  constructor() {
+    this.lista.push(new Carro(1, 'Fiesta'));
+    this.lista.push(new Carro(2, 'Focus'));
+    this.lista.push(new Carro(3, 'Fusion'));
+    this.lista.push(new Carro(4, 'Ka'));
+  }
+
+  editar() {
+    alert('Editar');
+  }
+
+  excluir() {
+    alert('Excluir');
+  }
 }
